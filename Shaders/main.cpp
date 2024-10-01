ff5e407d34d4b4ca4c0bcb20f63628a4a86cdc30
Rascal0902 <geonho050902@gmail.com>
2024-10-01 11:32:13 +0900

    Shader

---
+++ Shaders/main.cpp
@@ -0,0 +1,125 @@
+// System headers
+#include <iostream>
+
+// Vendor headers
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+// OpenGL callback functions
+namespace gl {
+void APIENTRY DebugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
+                                   GLsizei length, const GLchar* message, const void* user_param);
+}
+
+// GLFW callback functions
+namespace glfw {
+void CursorPosCallback(GLFWwindow* window, double xpos, double ypos);
+void ErrorCallback(int error_code, const char* description);
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+}
+
+
+int main() {
+  // Set GLFW error callback
+  (void)glfwSetErrorCallback(glfw::ErrorCallback);
+
+  // Initialize GLFW
+  if (!glfwInit()) {
+    std::cerr << "glfwInit failed" << std::endl;
+    return 1;
+  }
+
+  // Configure window hints
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+
+  // Initialize window
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "Shaders", nullptr, nullptr);
+
+  if (!window) {
+    std::cerr << "glfwCreateWindow failed" << std::endl;
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  // Set remaining GLFW callbacks
+  (void)glfwSetCursorPosCallback(window, glfw::CursorPosCallback);
+  (void)glfwSetFramebufferSizeCallback(window, glfw::FramebufferSizeCallback);
+  (void)glfwSetKeyCallback(window, glfw::KeyCallback);
+  (void)glfwSetMouseButtonCallback(window, glfw::MouseButtonCallback);
+  (void)glfwSetScrollCallback(window, glfw::ScrollCallback);
+
+  if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "gladLoadGL failed" << std::endl;
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 1;
+  }
+
+  // Enable and set OpenGL debug message callback
+  glEnable(GL_DEBUG_OUTPUT);
+  glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+  glDebugMessageCallback(gl::DebugMessageCallback, nullptr);
+
+  // Enable remaining OpenGL capabilities
+  glEnable(GL_DEPTH_TEST);
+
+  // Main loop
+  while (!glfwWindowShouldClose(window)) {
+    // Handle events
+    glfwPollEvents();
+
+    // Update
+
+    // Render
+    glClearColor(0.0F, 0.0F, 0.0F, 0.0F);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Swap buffers
+    glfwSwapBuffers(window);
+  }
+
+  // Cleanup
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
+
+// Debug callback functions
+namespace gl {
+void APIENTRY DebugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
+                                   GLsizei length, const GLchar* message, const void* user_param) {
+  std::cerr << "(" << id << ") " << message << std::endl;
+}
+}
+
+// GLFW callback functions
+namespace glfw {
+void CursorPosCallback(GLFWwindow* window, double xpos, double ypos) {
+}
+
+void ErrorCallback(int error_code, const char* description) {
+  std::cerr << "(" << error_code << ") " << description << std::endl;
+}
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+}
+
+void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+}
+
+void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
+}
+}
