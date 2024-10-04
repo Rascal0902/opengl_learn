658a3a55b33f6de1f31341363a56c2c9f5cb9913
Rascal0902 <geonho050902@gmail.com>
2024-10-04 13:13:24 +0900

    Transformations

---
+++ Transformations/cmake-build-debug/CMakeFiles/Transformations.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Transformations.dir/glad/src/glad.c.obj"
+  "CMakeFiles/Transformations.dir/glad/src/glad.c.obj.d"
+  "CMakeFiles/Transformations.dir/main.cpp.obj"
+  "CMakeFiles/Transformations.dir/main.cpp.obj.d"
+  "Transformations.exe"
+  "Transformations.exe.manifest"
+  "Transformations.pdb"
+  "libTransformations.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Transformations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
